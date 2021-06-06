5635cb11a6ce8070efc9212890bf2d0e9133afff
mkonnerth <matkonnerth@gmail.com>
2021-06-06 18:14:31 +0200

    own namespace for statemachine impl

--- main.cpp
+++ main.cpp
@@ -9,7 +9,48 @@ struct overload : Ts...
 };
 template <class... Ts>
 overload(Ts...) -> overload<Ts...>;
+template <typename Derived, typename StateVariant>
+class fsm
+{
+  public:
+    const StateVariant &get_state() const { return m_state; }
+    StateVariant &get_state() { return m_state; }
 
+    template <typename Event>
+    void dispatch(Event &&event)
+    {
+        Derived &child = static_cast<Derived &>(*this);
+        auto new_state = std::visit(
+            [&](auto &s) -> std::optional<StateVariant> {
+                return child.on_event(s, std::forward<Event>(event));
+            },
+            m_state);
+        if (new_state)
+        {
+            transitionDone(m_state, *new_state);
+            m_state = *std::move(new_state);
+        }
+        else
+        {
+            transitionError(m_state);
+        }
+    }
+
+  private:
+    StateVariant m_state;
+    void transitionDone(const StateVariant &from, const StateVariant &to)
+    {
+        std::cout << "transition from " << StateName(from) << " to "
+                  << StateName(to) << "\n";
+    }
+
+    void transitionError(const StateVariant &s)
+    {
+        std::cout << "transition error, stay in state " << StateName(s) << "\n";
+    }
+};
+namespace door
+{
 struct Opened
 {
 };
@@ -22,22 +63,11 @@ struct Locked
 {
     int key;
 };
-
-using State = std::variant<Opened, Closed, Locked>;
-
-std::string StateName(const State &state)
-{
-    std::string name;
-    std::visit(overload{[&](const Opened &) { name = "Opened"; },
-                        [&](const Closed &) { name = "Closed"; },
-                        [&](const Locked &) { name = "Locked"; }},
-               state);
-    return name;
-}
-
 struct Close
 {
 };
+
+using State = std::variant<Opened, Closed, Locked>;
 
 struct Open
 {
@@ -53,50 +83,17 @@ struct Unlock
     int key;
 };
 
-using ExternalEvent = std::variant<Close, Open, Lock, Unlock>;
-
-template <typename Derived, typename StateVariant>
-class fsm
+std::string StateName(const State &state)
 {
-  public:
-    const StateVariant &get_state() const { return state_; }
-    StateVariant &get_state() { return state_; }
+    std::string name;
+    std::visit(overload{[&](const Opened &) { name = "Opened"; },
+                        [&](const Closed &) { name = "Closed"; },
+                        [&](const Locked &) { name = "Locked"; }},
+               state);
+    return name;
+}
 
-    template <typename Event>
-    void dispatch(Event &&event)
-    {
-        Derived &child = static_cast<Derived &>(*this);
-        auto new_state = std::visit(
-            [&](auto &s) -> std::optional<StateVariant> {
-                return child.on_event(s, std::forward<Event>(event));
-            },
-            state_);
-        if (new_state)
-        {
-            transitionDone(state_, *new_state);
-            state_ = *std::move(new_state);
-        }
-        else
-        {
-            transitionError(state_);
-        }
-    }
-
-  private:
-    StateVariant state_;
-    void transitionDone(const StateVariant &from, const StateVariant &to)
-    {
-        std::cout << "transition from " << StateName(from) << " to "
-                  << StateName(to) << "\n";
-    }
-
-    void transitionError(const StateVariant &s)
-    {
-        std::cout << "transition error, stay in state " << StateName(s) << "\n";
-    }
-};
-
-class Door : public fsm<Door, State>
+class DoorFsm : public fsm<DoorFsm, State>
 {
   public:
     auto on_event(Opened &, const Close &) { return Closed{}; }
@@ -116,10 +113,13 @@ class Door : public fsm<Door, State>
         return std::nullopt;
     }
 };
+} // namespace Door
 
 int main(int, char *[])
 {
-    Door door{};
+    using namespace door;
+    
+    DoorFsm door{};
     door.dispatch(Close{});
     door.dispatch(Lock{123});
     door.dispatch(Unlock{23});
